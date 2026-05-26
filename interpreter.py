b665d6eae1b89fc180161d7a1651badd972d07b0
Alex Dadukin <alexanderdadukin@gmail.com>
2026-05-26 12:52:30 +0100

    Initial commit: add plus parsing

---
+++ interpreter.py
@@ -0,0 +1,49 @@
+from token import Token, EOF, INTEGER, PLUS
+from typing import Optional
+
+class Interpreter:
+    def __init__(self, text: str):
+        self.text = text
+        self.pos = 0
+        self.current_token: Optional[Token] = None
+
+    def error(self):
+        raise Exception("Cannot parse input.")
+
+    def get_next_token(self):
+        text = self.text
+
+        if self.pos >= len(text):
+            return Token(EOF, None)
+
+        current_char = text[self.pos]
+        self.pos += 1
+
+        if current_char.isdigit():
+            return Token(INTEGER, int(current_char))
+        elif current_char == "+":
+            return Token(PLUS, current_char)
+
+        self.error()
+
+    def consume(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+            return
+
+        self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token()
+
+        left = self.current_token
+        self.consume(INTEGER)
+
+        op = self.current_token
+        self.consume(PLUS)
+
+        right = self.current_token
+        self.consume(INTEGER)
+
+        result = left.value + right.value
+        return result
---
+++ token.py
@@ -0,0 +1,12 @@
+INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, {self.value})"
+
+    def __repr__(self):
+        return str(self)
